cceed4fe9c809a682b49d41873f104fabaf0fc47
Dennis Palucki <paluckide@yahoo.com>
2023-01-15 05:43:16 -0600

    Finish challenge three

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -7,7 +7,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 ------------------------------------------------------------------------------------------------ */
 
 const addTwo = (arr) => {
-  // Solution code here...
+  let newArr = [];
+  for(let num of arr){
+    newArr.push(num + 2);
+  }
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -19,7 +23,7 @@ For example, typeNum([1, 'bob' ,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const typeNum = (arr) => {
-  // Solution code here...
+  return arr.filter(Number.isInteger);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -31,7 +35,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter(name => name.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -43,7 +47,7 @@ For example, oddValues([1,2,3]) returns [1,3].
 ------------------------------------------------------------------------------------------------ */
 
 const oddValues = (arr) => {
-  // Solution code here...
+  return arr.filter(num => num % 2 === 1);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -55,7 +59,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter(item => !forbiddenValues.includes(item));
 };
 
 /* ------------------------------------------------------------------------------------------------
